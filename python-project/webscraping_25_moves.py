38c83c79a74450dac364b48b579e667698310a16
truongvude <truongvude@outlook.com>
2024-05-22 10:18:06 +0700

    add example and practice files

---
+++ python-project/webscraping_25_moves.py
@@ -0,0 +1,33 @@
+import requests
+import sqlite3
+import pandas as pd
+from bs4 import BeautifulSoup
+
+
+url = "https://web.archive.org/web/20230902185655/https://en.everybodywiki.com/100_Most_Highly-Ranked_Films"
+db_name = "Movies.db"
+table_name = "Top_25_Rotten"
+csv_path = "/home/project/top_25_films.csv"
+df = pd.DataFrame(columns=["Film", "Year", "Rotten Tomatoes' Top 100"])
+count = 0
+
+html_page = requests.get(url).text
+data = BeautifulSoup(html_page, "html_parser")
+
+tables = data.find_all("tbody")
+rows = tables[0].find_all("tr")
+
+for row in rows:
+    if count < 25:
+        col = rows.find_all("td")
+        if len(col) > 0:
+            data_dict = {"Film": col[1].contents[0],
+                         "Year": col[2].contents[0],
+                         "Rotten Tomatoes": col[3].contents[0]}
+            df1 = pd.DataFrame(data_dict, index=[0])
+            df = pd.concat([df, df1], ignore_index=True)
+            count += 1
+    else:
+        break
+
+print(df)
