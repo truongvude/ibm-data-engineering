5c4d111339ff6972497470cdf81cb19f093dbf1c
truongvude <truongvude@outlook.com>
2024-08-26 23:14:19 +0700

    add module 1 files

---
+++ python-project-for-data-engineering/project/webscrapting_movies_modify.py
@@ -0,0 +1,36 @@
+import pandas as pd
+import requests
+from bs4 import BeautifulSoup
+import sqlite3
+
+url = "https://web.archive.org/web/20230902185655/https://en.everybodywiki.com/100_Most_Highly-Ranked_Films"
+table_name = "Top_25"
+db_name = "Movies.db"
+csv_path = "./top_25_films.csv"
+df = pd.DataFrame(columns=["Film", "Year", "Rotten Tomatoes' Top 100"])
+count = 0
+
+html_page = requests.get(url).text
+data = BeautifulSoup(html_page, "html.parser")
+
+table = data.find_all("tbody")
+rows = table[0].find_all("tr")
+
+for row in rows:
+    if count < 25:
+        col = row.find_all("td")
+        if len(col) != 0:
+            data_dict = {"Film": col[1].contents[0], "Year": col[2].contents[0], "Rotten Tomatoes' Top 100": col[3].contents[0]}
+            df1 = pd.DataFrame(data_dict, index=[0])
+            df = pd.concat([df, df1], ignore_index=True)
+            count += 1
+    else:
+        break
+
+df.to_csv(csv_path)
+
+conn = sqlite3.connect(db_name)
+df.to_sql(table_name, conn, if_exists="replace", index=False)
+conn.close()
+
+print(df[df["Year"] >= "2000"])
